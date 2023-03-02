5b0940abd30c0744e6a7a828df33edd7a95e9de7
Logan Marchione <logan@loganmarchione.com>
2023-03-02 14:46:20 -0500

    Rename DNS resources

--- terraform/production/aws_email.tf
+++ terraform/production/aws_email.tf
@@ -7,21 +7,21 @@
 ########################################
 
 # Create the domain
-resource "aws_ses_domain_identity" "domain_identity" {
+resource "aws_ses_domain_identity" "loganmarchione_com" {
   domain = aws_route53_zone.loganmarchione_com.name
 }
 
 # Verify the domain
-resource "aws_ses_domain_identity_verification" "domain_verification" {
-  domain = aws_ses_domain_identity.domain_identity.id
+resource "aws_ses_domain_identity_verification" "loganmarchione_com" {
+  domain = aws_ses_domain_identity.loganmarchione_com.id
   depends_on = [
-    aws_route53_record.amazonses_verification
+    aws_route53_record.loganmarchione_com_txt_ses
   ]
 }
 
 # DKIM
-resource "aws_ses_domain_dkim" "domain_dkim" {
-  domain = aws_ses_domain_identity.domain_identity.domain
+resource "aws_ses_domain_dkim" "loganmarchione_com" {
+  domain = aws_ses_domain_identity.loganmarchione_com.domain
 }
 
 ########################################
@@ -29,21 +29,21 @@ resource "aws_ses_domain_dkim" "domain_dkim" {
 ########################################
 
 # Create the domain
-resource "aws_ses_domain_identity" "domain_identity2" {
+resource "aws_ses_domain_identity" "homelab_domain" {
   domain = aws_route53_zone.homelab_domain.name
 }
 
 # Verify the domain
-resource "aws_ses_domain_identity_verification" "domain_verification2" {
-  domain = aws_ses_domain_identity.domain_identity2.id
+resource "aws_ses_domain_identity_verification" "homelab_domain" {
+  domain = aws_ses_domain_identity.homelab_domain.id
   depends_on = [
-    aws_route53_record.amazonses_verification2
+    aws_route53_record.homelab_domain_txt_ses
   ]
 }
 
 # DKIM
-resource "aws_ses_domain_dkim" "domain_dkim2" {
-  domain = aws_ses_domain_identity.domain_identity2.domain
+resource "aws_ses_domain_dkim" "homelab_domain" {
+  domain = aws_ses_domain_identity.homelab_domain.domain
 }
 
 ################################################################################
@@ -55,24 +55,24 @@ resource "aws_ses_domain_dkim" "domain_dkim2" {
 ########################################
 
 # Create DNS records for SES
-resource "aws_route53_record" "amazonses_verification" {
+resource "aws_route53_record" "loganmarchione_com_txt_ses" {
   zone_id = aws_route53_zone.loganmarchione_com.id
-  name    = "_amazonses.${aws_ses_domain_identity.domain_identity.domain}"
+  name    = "_amazonses.${aws_ses_domain_identity.loganmarchione_com.domain}"
   type    = "TXT"
   ttl     = "3600"
   records = [
-    aws_ses_domain_identity.domain_identity.verification_token
+    aws_ses_domain_identity.loganmarchione_com.verification_token
   ]
 }
 
 # Create DNS records for DKIM
-resource "aws_route53_record" "dkim_record" {
+resource "aws_route53_record" "loganmarchione_com_cname_ses" {
   zone_id = aws_route53_zone.loganmarchione_com.id
-  name    = "${element(aws_ses_domain_dkim.domain_dkim.dkim_tokens, count.index)}._domainkey"
+  name    = "${element(aws_ses_domain_dkim.loganmarchione_com.dkim_tokens, count.index)}._domainkey"
   type    = "CNAME"
   ttl     = "3600"
   records = [
-    "${element(aws_ses_domain_dkim.domain_dkim.dkim_tokens, count.index)}.dkim.amazonses.com"
+    "${element(aws_ses_domain_dkim.loganmarchione_com.dkim_tokens, count.index)}.dkim.amazonses.com"
   ]
   count = 3
 }
@@ -82,24 +82,24 @@ resource "aws_route53_record" "dkim_record" {
 ########################################
 
 # Create DNS records for SES
-resource "aws_route53_record" "amazonses_verification2" {
+resource "aws_route53_record" "homelab_domain_txt_ses" {
   zone_id = aws_route53_zone.homelab_domain.id
-  name    = "_amazonses.${aws_ses_domain_identity.domain_identity2.domain}"
+  name    = "_amazonses.${aws_ses_domain_identity.homelab_domain.domain}"
   type    = "TXT"
   ttl     = "3600"
   records = [
-    aws_ses_domain_identity.domain_identity2.verification_token
+    aws_ses_domain_identity.homelab_domain.verification_token
   ]
 }
 
 # Create DNS records for DKIM
-resource "aws_route53_record" "dkim_record2" {
+resource "aws_route53_record" "homelab_domain_cname_ses" {
   zone_id = aws_route53_zone.homelab_domain.id
-  name    = "${element(aws_ses_domain_dkim.domain_dkim2.dkim_tokens, count.index)}._domainkey"
+  name    = "${element(aws_ses_domain_dkim.homelab_domain.dkim_tokens, count.index)}._domainkey"
   type    = "CNAME"
   ttl     = "3600"
   records = [
-    "${element(aws_ses_domain_dkim.domain_dkim2.dkim_tokens, count.index)}.dkim.amazonses.com"
+    "${element(aws_ses_domain_dkim.homelab_domain.dkim_tokens, count.index)}.dkim.amazonses.com"
   ]
   count = 3
 }
