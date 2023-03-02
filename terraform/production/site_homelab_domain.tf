16d4e584a125098526207e816a520fa7fd935ed5
Logan Marchione <logan@loganmarchione.com>
2023-03-02 12:10:31 -0500

    Add homelab domain NS records

--- terraform/production/site_homelab_domain.tf
+++ terraform/production/site_homelab_domain.tf
@@ -6,6 +6,15 @@ resource "aws_route53_zone" "homelab_domain" {
   name = var.homelab_domain
 }
 
+resource "aws_route53_record" "homelab_domain_nameservers" {
+  zone_id         = aws_route53_zone.homelab_domain.zone_id
+  name            = aws_route53_zone.homelab_domain.name
+  type            = "NS"
+  ttl             = "3600"
+  allow_overwrite = true
+  records         = aws_route53_zone.homelab_domain.name_servers
+}
+
 resource "aws_route53_record" "homelab_domain_caa" {
   zone_id = aws_route53_zone.homelab_domain.zone_id
   name    = ""
