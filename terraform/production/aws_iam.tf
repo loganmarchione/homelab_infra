36df0f5db7688a12ae6879a3b8c4dff47a894343
Logan Marchione <logan@loganmarchione.com>
2023-03-02 16:34:47 -0500

    Create access keys and policies

--- terraform/production/aws_iam.tf
+++ terraform/production/aws_iam.tf
@@ -2,18 +2,82 @@
 ### Create users
 ################################################################################
 
-# Create IAM user
+
+########################################
+### ACME
+########################################
+
+# Create IAM user for ACME
 resource "aws_iam_user" "acme" {
   name = "acme"
 }
 
-# Create IAM user
+resource "aws_iam_access_key" "acme" {
+  user = aws_iam_user.acme.name
+}
+
+output "acme_username" {
+  value     = aws_iam_access_key.acme.id
+  sensitive = true
+}
+
+output "acme_password" {
+  value     = aws_iam_access_key.acme.secret
+  sensitive = true
+}
+
+########################################
+### SES
+########################################
+
+# Create IAM user for postfixrelay
 resource "aws_iam_user" "postfixrelay" {
   name = "postfixrelay"
 }
 
 resource "aws_iam_access_key" "postfixrelay" {
   user = aws_iam_user.postfixrelay.name
+}
+
+output "postfixrelay_username" {
+  value     = aws_iam_access_key.postfixrelay.id
+  sensitive = true
+}
+
+# Need to get the SES SMTP password instead of the access key
+output "postfixrelay_password" {
+  value     = aws_iam_access_key.postfixrelay.ses_smtp_password_v4
+  sensitive = true
+}
+
+################################################################################
+### SES
+################################################################################
+
+resource "aws_iam_policy" "ses_send_only" {
+  name        = "SESSendOnly"
+  path        = "/"
+  description = "Policy to allow sending email via SES"
+
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Id" : "SESSendOnly",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Action" : [
+          "ses:SendRawEmail"
+        ],
+        "Resource" : "*"
+      }
+    ]
+  })
+}
+
+# Assign the policy
+resource "aws_iam_user_policy_attachment" "ses_send_only" {
+  user       = aws_iam_user.postfixrelay.name
+  policy_arn = aws_iam_policy.ses_send_only.arn
 }
 
 ################################################################################
