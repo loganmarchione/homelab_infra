7eaa28fca5fd52d17e2f8a8d05da3b64fd52ddcf
Logan Marchione <logan@loganmarchione.com>
2023-03-28 21:05:13 -0400

    Add KMS key

---
+++ terraform/production/kms.tf
@@ -0,0 +1,8 @@
+resource "aws_kms_key" "s3_bucket_encryption" {
+  description         = "KMS key used to encrypt S3 buckets"
+}
+
+resource "aws_kms_alias" "s3_bucket_encryption" {
+  name          = "alias/s3-bucket-encryption"
+  target_key_id = aws_kms_key.s3_bucket_encryption.key_id
+}
