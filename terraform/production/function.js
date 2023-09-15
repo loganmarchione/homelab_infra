dff957c341824b18e6b690dfab6d5823fc54dde8
Logan Marchione <logan@loganmarchione.com>
2023-09-15 16:19:49 -0400

    Update function

--- terraform/production/function.js
+++ terraform/production/function.js
@@ -2,6 +2,19 @@ function handler(event) {
     var request = event.request;
     var uri = request.uri;
 
+    // Rewrite URL
+    if (uri === "/resume") {
+        var response = {
+            statusCode: 301,
+            statusDescription: "Moved Permanently",
+            headers: {
+                "location": { "value": "https://loganmarchione.rocks/resumes/LoganMarchione_Resume_2023-03-31.pdf" }
+            }
+        }
+        return response;
+    }
+
+    // https://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/example-function-add-index.html
     // Check whether the URI is missing a file name.
     if (uri.endsWith('/')) {
         request.uri += 'index.html';
@@ -11,5 +24,6 @@ function handler(event) {
         request.uri += '/index.html';
     }
 
+    // If none of the above conditions are met, return the original request
     return request;
 }
