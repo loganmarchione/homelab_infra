b5c48e97161de58b6a19f959783d8d6f2e134921
Logan Marchione <logan@loganmarchione.com>
2023-09-24 16:02:34 -0400

    Move loganmarchione.com to module

--- terraform/production/site_loganmarchione_com.tf
+++ terraform/production/site_loganmarchione_com.tf
@@ -23,46 +23,6 @@ resource "aws_route53_record" "loganmarchione_com_nameservers" {
 ### All other records
 ########################################
 
-resource "aws_route53_record" "loganmarchione_com_a" {
-  zone_id = aws_route53_zone.loganmarchione_com.zone_id
-  name    = ""
-  type    = "A"
-  ttl     = "3600"
-  records = [digitalocean_droplet.web01.ipv4_address]
-}
-
-resource "aws_route53_record" "loganmarchione_com_aaaa" {
-  zone_id = aws_route53_zone.loganmarchione_com.zone_id
-  name    = ""
-  type    = "AAAA"
-  ttl     = "3600"
-  records = [digitalocean_droplet.web01.ipv6_address]
-}
-
-resource "aws_route53_record" "loganmarchione_com_a_www" {
-  zone_id = aws_route53_zone.loganmarchione_com.zone_id
-  name    = "www"
-  type    = "A"
-
-  alias {
-    name                   = "loganmarchione.com"
-    zone_id                = aws_route53_zone.loganmarchione_com.zone_id
-    evaluate_target_health = false
-  }
-}
-
-resource "aws_route53_record" "loganmarchione_com_aaaa_www" {
-  zone_id = aws_route53_zone.loganmarchione_com.zone_id
-  name    = "www"
-  type    = "AAAA"
-
-  alias {
-    name                   = "loganmarchione.com"
-    zone_id                = aws_route53_zone.loganmarchione_com.zone_id
-    evaluate_target_health = false
-  }
-}
-
 resource "aws_route53_record" "loganmarchione_com_mx_fastmail" {
   zone_id = aws_route53_zone.loganmarchione_com.zone_id
   name    = ""
@@ -115,13 +75,59 @@ resource "aws_route53_record" "loganmarchione_com_cname_fastmail3" {
   ]
 }
 
-resource "aws_route53_record" "loganmarchione_com_caa" {
-  zone_id = aws_route53_zone.loganmarchione_com.zone_id
-  name    = ""
-  type    = "CAA"
-  ttl     = "3600"
-  records = [
-    "0 issue \"letsencrypt.org\"",
-    "0 issuewild \"letsencrypt.org\""
-  ]
+################################################################################
+### Module for static site
+################################################################################
+
+module "static_site_loganmarchione_com" {
+  source = "github.com/loganmarchione/terraform-aws-static-site?ref=0.1.6"
+
+  providers = {
+    aws.us-east-1 = aws.us-east-1
+  }
+
+  # The domain name of the site (**MUST** match the Route53 hosted zone name (e.g., `domain.com`)
+  domain_name = "loganmarchione.com"
+
+  # Since this is a static site, we probably don't need versioning, since our source files are stored in git
+  bucket_versioning_logs = false
+  bucket_versioning_site = false
+
+  # CloudFront settings
+  cloudfront_compress                     = true
+  cloudfront_default_root_object          = "index.html"
+  cloudfront_enabled                      = true
+  cloudfront_function_create              = true
+  cloudfront_function_filename            = "function.js"
+  cloudfront_function_name                = "ReWrites"
+  cloudfront_http_version                 = "http2and3"
+  cloudfront_ipv6                         = true
+  cloudfront_price_class                  = "PriceClass_100"
+  cloudfront_ssl_minimum_protocol_version = "TLSv1.2_2021"
+  cloudfront_ttl_min                      = 3600
+  cloudfront_ttl_default                  = 86400
+  cloudfront_ttl_max                      = 2592000
+  cloudfront_viewer_protocol_policy       = "redirect-to-https"
+
+  # IAM
+  iam_policy_site_updating = true
+
+  # Upload default files
+  upload_index  = false
+  upload_robots = false
+  upload_404    = false
+}
+
+################################################################################
+### Module for GitHub OIDC role
+################################################################################
+
+module "iam_github_oidc_role_loganmarchione_com" {
+  source = "github.com/terraform-aws-modules/terraform-aws-iam?ref=v5.30.0//modules/iam-github-oidc-role"
+
+  name = "GitHubActionsOIDC-loganmarchione-com"
+  policies = {
+    SiteUpdating-loganmarchione-com = module.static_site_loganmarchione_com.site_updating_iam_policy_arn
+  }
+  subjects = ["loganmarchione/loganmarchione.com:*"]
 }
--- terraform/production/site_loganmarchione_dev.tf
+++ terraform/production/site_loganmarchione_dev.tf
@@ -24,7 +24,7 @@ resource "aws_route53_record" "loganmarchione_dev_nameservers" {
 ################################################################################
 
 module "static_site_loganmarchione_dev" {
-  source = "github.com/loganmarchione/terraform-aws-static-site?ref=0.1.3"
+  source = "github.com/loganmarchione/terraform-aws-static-site?ref=0.1.6"
 
   providers = {
     aws.us-east-1 = aws.us-east-1
@@ -66,7 +66,7 @@ module "static_site_loganmarchione_dev" {
 ### Module for GitHub OIDC role
 ################################################################################
 
-module "iam_github_oidc_role" {
+module "iam_github_oidc_role_loganmarchione_dev" {
   source = "github.com/terraform-aws-modules/terraform-aws-iam?ref=v5.30.0//modules/iam-github-oidc-role"
 
   name = "GitHubActionsOIDC-loganmarchione-dev"
