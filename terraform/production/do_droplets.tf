61df68e2cfc7c2c4a3b4d62e9d2ef7b6b1abb848
Logan Marchione <logan@loganmarchione.com>
2023-10-20 11:42:39 -0400

    Remove web01

--- terraform/production/do_droplets.tf
+++ terraform/production/do_droplets.tf
@@ -1,18 +1,3 @@
-resource "digitalocean_droplet" "web01" {
-  image      = var.do_image
-  name       = "web01.external.loganmarchione.com"
-  region     = var.do_region
-  size       = "s-1vcpu-1gb"
-  ipv6       = "true"
-  backups    = "false"
-  monitoring = "false"
-
-  lifecycle {
-    prevent_destroy = true
-    ignore_changes  = [image]
-  }
-}
-
 resource "digitalocean_droplet" "web02" {
   image      = var.do_image
   name       = "web02.external.mariapietropola.com"
