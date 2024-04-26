c217262297f2fcc63d76fc0d1ac9219b1b8baeab
Logan Marchione <logan@loganmarchione.com>
2024-04-26 15:18:16 -0400

    Bump module versions

--- terraform/production/aws_oidc.tf
+++ terraform/production/aws_oidc.tf
@@ -3,5 +3,5 @@
 ################################################################################
 
 module "oidc_provider" {
-  source = "github.com/terraform-aws-modules/terraform-aws-iam?ref=v5.33.0//modules/iam-github-oidc-provider"
+  source = "github.com/terraform-aws-modules/terraform-aws-iam?ref=v5.39.0//modules/iam-github-oidc-provider"
 }
