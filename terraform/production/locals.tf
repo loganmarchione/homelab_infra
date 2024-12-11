e44013d0a31b041ecb60550b1a99dff4dd5d4055
Logan Marchione <logan@loganmarchione.com>
2024-12-11 12:18:12 -0500

    Lowercase to prevent diff

--- terraform/production/locals.tf
+++ terraform/production/locals.tf
@@ -3,11 +3,11 @@ locals {
   hamradionewbie_com_github_pages_ipv4_addresses = ["185.199.108.153", "185.199.109.153", "185.199.110.153", "185.199.111.153"]
   hamradionewbie_com_github_pages_ipv6_addresses = ["2606:50c0:8000::153", "2606:50c0:8001::153", "2606:50c0:8002::153", "2606:50c0:8003::153"]
   loganandmaria_mx_records = [
-    { priority = 1, content = "ASPMX.L.GOOGLE.COM" },
-    { priority = 5, content = "ALT1.ASPMX.L.GOOGLE.COM" },
-    { priority = 5, content = "ALT2.ASPMX.L.GOOGLE.COM" },
-    { priority = 10, content = "ALT3.ASPMX.L.GOOGLE.COM" },
-    { priority = 10, content = "ALT4.ASPMX.L.GOOGLE.COM" },
+    { priority = 1, content = "aspmx.l.google.com" },
+    { priority = 5, content = "alt1.aspmx.l.google.com" },
+    { priority = 5, content = "alt2.aspmx.l.google.com" },
+    { priority = 10, content = "alt3.aspmx.l.google.com" },
+    { priority = 10, content = "alt4.aspmx.l.google.com" },
     { priority = 15, content = "gubaxfbs3pclxek6tkmakpeeye2l2tveafmdf5xkhumda67hws4q.mx-verification.google.com" }
   ]
 }
