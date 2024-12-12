61e4d4ecdcacc003cb4c93dbb2aecac62cff1007
Logan Marchione <logan@loganmarchione.com>
2024-12-12 15:38:31 -0500

    Remove web02

--- terraform/production/do_droplets.tf
+++ terraform/production/do_droplets.tf
@@ -1,19 +1,3 @@
-resource "digitalocean_droplet" "web02" {
-  # doctl compute image list --public
-  image      = "debian-11-x64"
-  name       = "web02.external.mariapietropola.com"
-  region     = var.do_region
-  size       = "s-1vcpu-1gb"
-  ipv6       = "true"
-  backups    = "false"
-  monitoring = "false"
-
-  lifecycle {
-    prevent_destroy = true
-    ignore_changes  = [image]
-  }
-}
-
 resource "digitalocean_droplet" "web01" {
   # doctl compute image list --public
   image      = "debian-12-x64"
