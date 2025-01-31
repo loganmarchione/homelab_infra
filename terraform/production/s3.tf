3415c4dcab5d0332c3efe4d7c12a0cda3c55c426
Logan Marchione <logan@loganmarchione.com>
2025-01-31 13:33:08 -0500

    Remove state logging bucket

--- terraform/production/s3.tf
+++ terraform/production/s3.tf
@@ -3,15 +3,12 @@
 ################################################################################
 
 ########################################
-### Logging bucket
+### Terraform state bucket
 ########################################
 
-# Create a bucket
-# Ignore KICS scan: S3 Bucket Logging Disabled
-# Reason: This bucket is the logging bucket
-# kics-scan ignore-line
-resource "aws_s3_bucket" "loganmarchione_logging" {
-  bucket = "loganmarchione-logging"
+# Create a bucket for Terraform state
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "loganmarchione-terraform-state"
 
   lifecycle {
     prevent_destroy = true
@@ -19,14 +16,14 @@ resource "aws_s3_bucket" "loganmarchione_logging" {
 }
 
 # Make the bucket private
-resource "aws_s3_bucket_acl" "loganmarchione_logging" {
-  bucket = aws_s3_bucket.loganmarchione_logging.id
-  acl    = "log-delivery-write"
+resource "aws_s3_bucket_acl" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.id
+  acl    = "private"
 }
 
 # Enable bucket versioning
-resource "aws_s3_bucket_versioning" "loganmarchione_logging" {
-  bucket = aws_s3_bucket.loganmarchione_logging.id
+resource "aws_s3_bucket_versioning" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.id
 
   versioning_configuration {
     status = "Enabled"
@@ -34,8 +31,8 @@ resource "aws_s3_bucket_versioning" "loganmarchione_logging" {
 }
 
 # Bucket encryption
-resource "aws_s3_bucket_server_side_encryption_configuration" "loganmarchione_logging" {
-  bucket = aws_s3_bucket.loganmarchione_logging.bucket
+resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.bucket
 
   rule {
     apply_server_side_encryption_by_default {
@@ -44,33 +41,39 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "loganmarchione_lo
   }
 }
 
+# Enable bucket logging
+#resource "aws_s3_bucket_logging" "terraform_state" {
+#  bucket = aws_s3_bucket.terraform_state.id
+#
+#  target_bucket = aws_s3_bucket.terraform_state_logging.id
+#  target_prefix = "log/"
+#}
+
 # Make sure the bucket is not public
-resource "aws_s3_bucket_public_access_block" "loganmarchione_logging" {
-  bucket                  = aws_s3_bucket.loganmarchione_logging.id
+resource "aws_s3_bucket_public_access_block" "s3_terraform_state" {
+  bucket                  = aws_s3_bucket.terraform_state.id
   block_public_acls       = true
   ignore_public_acls      = true
   block_public_policy     = true
   restrict_public_buckets = true
 }
 
-# Bucket lifecycle
-resource "aws_s3_bucket_lifecycle_configuration" "loganmarchione_logging" {
-  bucket = aws_s3_bucket.loganmarchione_logging.id
+resource "aws_s3_bucket_lifecycle_configuration" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.id
 
   rule {
     id = "30d_move_to_S3_IA_and_1y_delete_old_objects"
-    filter {
-      prefix = "s3_${aws_s3_bucket.terraform_state.id}/"
-    }
     # expire (delete) objects older than 1 year
-    expiration {
-      days = 365
+    noncurrent_version_expiration {
+      newer_noncurrent_versions = 10
+      noncurrent_days           = 365
     }
     status = "Enabled"
     # transition to STANDARD_ID after 30 days
-    transition {
-      days          = 30
-      storage_class = "STANDARD_IA"
+    noncurrent_version_transition {
+      newer_noncurrent_versions = 10
+      noncurrent_days           = 30
+      storage_class             = "STANDARD_IA"
     }
   }
 }
